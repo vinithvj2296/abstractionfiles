8c10893b899fe29f3bdbd744a8900df580087a62
vinithvj2296 <110960270+vinithvj2296@users.noreply.github.com>
2022-08-12 19:33:48 +0530

    Update netflix.java

--- src/org/ott/netflix.java
+++ src/org/ott/netflix.java
@@ -5,7 +5,7 @@ public abstract class netflix {
 	public abstract void monthlyplan();
 	
 	public void yearlyplan(){
-		System.out.println("1499");
+		System.out.println("2499");
 		
 	}
 	public static void main(String[] args) {
